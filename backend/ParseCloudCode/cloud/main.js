89bde294080bb966f4b839d1d45940972ba8caeb
Elie Javice <ejavice@me.com>
2013-09-07 02:33:32 -0400

    twilio initial

--- backend/ParseCloudCode/cloud/main.js
+++ backend/ParseCloudCode/cloud/main.js
@@ -1,6 +1,31 @@
+var twilio = require("twilio");
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var twilioSid = "AC373eaacb72f601e5fe64a9278197d0c3";
+var twilioAuthToken = "46653b52833b0a2757d15e167610e5b6";
+
+twilio.initialize(twilioSid, twilioAuthToken);
+
+
+// Twilio Test
+Parse.Cloud.define("testTwilio", function(request, response) {
+	twilio.sendSMS({
+		From: "+19142886105",
+		To: "+19145759309",
+		Body: "Hello from Cloud Code!"
+	}, {
+	success: function(httpResponse) {
+		console.log(httpResponse);
+		response.success("SMS sent!");
+	},
+	error: function(httpResponse) {
+		console.error(httpResponse);
+		response.error("Uh oh, something went wrong");
+	}
+	});
 });
+
+// test cloud code
+Parse.Cloud.define("cloudTest", function(request, response) {
+  response.success("true");
+});
+	
